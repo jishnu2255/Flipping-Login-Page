fc665b2d598cd4d5666456b4e6dcc4949ccd32d0
Jishnu <jishnujalajahari123456@gmail.com>
2022-01-23 23:33:33 -0800

    first commit

---
+++ login.js
@@ -0,0 +1,16 @@
+function check()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if (username="jishnu"&& password ="2255")
+    {
+        alert("Login Successfull");
+        return false;
+
+    }
+    else
+    {
+        alert("Incorrect credentils");
+    }
+}
+}
